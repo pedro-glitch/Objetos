b76de62a31a6b9baa336f7b9dbeff8bc127174ad
pedro-glitch <pcaldas2011@gmail.com>
2025-09-15 11:30:51 -0300

    Atualiza script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,59 @@
-console.log("hello world!")
+/*let carro = {
+    marca: "toyota",
+    modelo: "corolla",
+    ano: 2022,
+    cor: "preto"
+};
+console.log(carro.marca);*/
+
+/*let carro = {
+    marca: "toyota",
+    modelo: "corolla",
+    ano: 2022,
+    cor: "preto"
+};
+console.log(carro);
+
+carro.portas = 4;
+carro.cor = "Prata";
+console.log(carro)*/
+
+/*let carro ={
+    marca: "Honda",
+    modelo: "Civic",
+    ligar: function(){
+        console.log("O carro está ligado!");
+    }
+};
+
+carro.ligar();*/
+
+/*let pessoa = {
+    nome: "ANA",
+    idade: 25,
+    endereço: {
+        rua: "Av. Brasil",
+        numero: 123
+    }
+};
+
+console.log(pessoa.endereço.rua);*/
+
+/*let aluno ={
+    nome: "Pedro",
+    idade: 31,
+    curso: "BFD"
+};
+console.log(aluno);*/
+
+/*let aluno ={
+    nome: "Pedro",
+    idade: 31,
+    curso: "BFD",
+    apresentar(){
+        console.log("olá meu nome é Pedro e estudo na BFD")
+    }
+};
+
+aluno.apresentar();*/
+
